11e5db825d420b1df5d164518499d0d41b134a07
BeakSungHyun <ces0135@naver.com>
2022-03-29 00:03:45 +0900

    checkbox 클릭 시 addEventListenrer를 이용하여 alert가 뜨는지 확인해보았다.
    checkbox를 클릭했을 때 작동하지 않으므로 수정이 필요하다.

--- TestCode/templete.js
+++ TestCode/templete.js
@@ -1,6 +1,10 @@
 //https://wikidocs.net/111736
 //addEventListener 사용법, event 종류
 //https://kyounghwan01.github.io/blog/JS/JSbasic/addEventListener/
+//텍스트 변경
+//https://www.delftstack.com/ko/howto/javascript/javascript-change-text/#javascript%EC%97%90%EC%84%9C-createtextnode-%ED%95%A8%EC%88%98%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%EC%9A%94%EC%86%8C%EC%9D%98-%ED%85%8D%EC%8A%A4%ED%8A%B8-%EB%B3%80%EA%B2%BD
+//텍스트 스타일 변경
+//https://www.daleseo.com/css-typography/
 let field = document.querySelector('#todo-list');  //to do list가 추가되는 곳
 
 let newInput = document.querySelector('#new-todo-title');
@@ -9,6 +13,9 @@ newInput.addEventListener('keypress', function(e) {
         addDiv();
     }
 });
+
+let forCheckBox = document.getElementById('listCheckBox');
+forCheckBox.addEventListener('click', isChecked());
 
 function addDiv() {
     let cntDiv = document.getElementsByClassName('newDiv').length + 1;  //생성된 div의 개수
@@ -20,7 +27,7 @@ function addDiv() {
     let newToDo = document.querySelector('#new-todo-title').value;  //입력받는 to do
     
     var str = '';  //추가될 to do list의 content
-    str += "<input type=" + "'checkbox'/>";  //체크 박스
+    str += "<input id=" + "listCheckBox" + " type=" + "'checkbox'/>";  //체크 박스
     str += newToDo;  //새로 입력받은 항목
     str += "<button class=" + "delBtn" + " id=" + "delBtn" + cntDiv + " onclick=" + "delDiv(this.id)" + ">X</button>";  //삭제 버튼
 
@@ -38,4 +45,18 @@ function delDiv(curId) {
     }
 
     field.removeChild(document.getElementById('newDiv' + delId));
+}
+
+function isChecked() {
+    let checkChBox = forCheckBox.checked;
+
+    alert(checkChBox);
+    alert("check");
+    if(checkChBox === true) {
+
+    }
+    
+    if(checkChBox === false) {
+
+    }
 }
